d2b30ed75d300d75a74bd507a2a529d44ea7cc84
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2022-11-22 21:03:16 +0530

    reducer and datalayer injected

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "../spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("🧑", user);
+
+        // spotify.getUserPlaylists().then((playlists) => {
+        //   console.log("🎶", playlists);
+        // });
+      });
+    }
+
+    console.log("I have a token 👉", token);
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
+import { DataLayer } from "./components/DataLayer";
+import reducer, { initialState } from "./reducer";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
   </React.StrictMode>
 );
 
