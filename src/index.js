d6863a3ecb9300ff59c7e57ba6639b7ee27eb1e9
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2022-11-27 16:27:40 +0530

    spotify-clone-v1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./components/App";
 import { DataLayer } from "./components/DataLayer";
 import reducer, { initialState } from "./reducer";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,8 +13,3 @@ root.render(
     </DataLayer>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
